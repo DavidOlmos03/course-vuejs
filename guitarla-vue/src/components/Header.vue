bcbc24eab352c107796beea296792f0c4a9fe156
Juan David Ruiz Olmos <juanda_201001@hotmail.com>
2024-04-14 00:04:06 -0500

    first commit to firstProject

---
+++ guitarla-vue/src/components/Header.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { reactive } from 'vue';
+
+    const obj = reactive({
+        disponible: 'true',
+        cantidad: 4
+    });
+
+    console.log(obj.cantidad)
+</script>
+<template>
+    <div>
+        {{ obj.cantidad }}
+    </div>
+</template>
+
+<style>
+
+</style>
