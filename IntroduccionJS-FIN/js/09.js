f7b6ff48fd109631db59e07100ca8f22e53a384e
Juan David Ruiz Olmos <101577673+DavidOlmos03@users.noreply.github.com>
2024-04-20 01:07:34 -0500

    Delete IntroduccionJS-FIN directory

--- IntroduccionJS-FIN/js/09.js
+++
@@ -1,5 +0,0 @@
-// Arrays o Arreglos
-const tecnologias = ['Vue.js', 'JavaScript', 'Node.js', 30, 20, true, {}]
-
-
-console.log(tecnologias)
--- IntroduccionJS-FIN/js/20.js
+++
@@ -1,9 +0,0 @@
-// Operador ternario - condicion ? si se cumple : si no se cumple
-
-const disponible = 100
-const total = 500
-const tarjeta = true
-
-disponible > total && tarjeta ? 
-        console.log('Si puedes pagar con saldo o tarjeta') :
-        console.log('No puedes pagar')
