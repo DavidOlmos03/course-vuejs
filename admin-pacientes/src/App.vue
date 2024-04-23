24e8280ba30e0979c11003260f992f10356c6baa
Juan David Ruiz Olmos <juanda_201001@hotmail.com>
2024-04-23 04:45:46 -0500

    second project finished

--- admin-pacientes/src/App.vue
+++ admin-pacientes/src/App.vue
@@ -1,13 +1,110 @@
 <script setup>
+  import {ref, reactive,watch,onMounted} from 'vue'
+  import {uid} from 'uid'
   import Header from './components/Header.vue'
   import Formulario from './components/Formulario.vue'
+  import Paciente from './components/Paciente.vue'
+
+  //array
+  const pacientes = ref([])
+
+  watch(pacientes,()=>{
+    guardarLocalStorage()
+  },{
+    deep:true
+  })
+
+  onMounted(()=>{    //metodo de ciclo de vida onMounted
+    const pacientesStorage = localStorage.getItem('pacientes')
+    if(pacientesStorage){
+      pacientes.value = JSON.parse(pacientesStorage)    //parse para pasar de JSON a arreglo
+    }
+  })
+
+  const guardarLocalStorage = ()=>{
+    localStorage.setItem('pacientes',JSON.stringify(pacientes.value))
+  }
+  //object
+  const paciente = reactive({
+        id: null,
+        nombre: '',
+        propietario: '',
+        email: '',
+        alta: '',
+        sintomas: ''
+    })
+
+  
+  const guardarPaciente = () =>{
+    if(paciente.id){ //En caso de que el id sea diferente de null, es porque el paciente ya esta creado y lo que se va a hacer es EDITARLO
+      const {id} = paciente
+      const i = pacientes.value.findIndex((pacienteState) => pacienteState.id === id )
+      pacientes.value[i] = {...paciente}
+
+    }else{   //En caso de el id ser null es porque no se ha creado el usuario, entonces se esta REGISTRANDO
+      pacientes.value.push({
+      ...paciente,
+      id: uid()
+      }) 
+    }
+    //Reiniciar paciente
+    // paciente.nombre= '',
+    // paciente.propietario= '',
+    // paciente.email= '',
+    // paciente.alta= '',
+    // paciente.sintomas= ''
+
+    //Otra forma
+    Object.assign(paciente,{
+      nombre: '',
+      propietario: '',
+      email: '',
+      alta: '',
+      sintomas: '',
+      id: null
+    })
+  }
+
+  const actualizarPaciente = (id)=>{
+    const pacienteEditar = pacientes.value.filter(paciente => paciente.id === id)[0]
+    Object.assign(paciente,pacienteEditar)
+  }
+
+  const eliminarPaciente = (id) =>{
+    pacientes.value = pacientes.value.filter(pacienteState => pacienteState.id !== id)
+  }
 </script>
 
 <template>
   <div class="container mx-auto mt-20">
     <Header />
     <div class="mt-12 md:flex">
-      <Formulario />
+      <Formulario 
+        v-model:nombre="paciente.nombre"
+        v-model:propietario="paciente.propietario"
+        v-model:email="paciente.email"
+        v-model:alta="paciente.alta"
+        v-model:sintomas="paciente.sintomas"
+        @guardar-paciente = "guardarPaciente"
+        :id = "paciente.id"
+      />
+      <div class="md:w-1/2 md:h-screen overflow-y-scroll">
+        <h3 class="font-black text-3xl text-center">Administra tus pacientes</h3>
+        
+        <div v-if="pacientes.length > 0">
+          <p class="text-lg mt-5 text-center mb-10">
+            Información de
+            <span class="text-indigo-600 font-bold">Pacientes</span>  
+          </p>
+          <Paciente 
+          v-for="paciente in pacientes"
+          :paciente="paciente"
+          @actualizar-paciente = "actualizarPaciente"
+          @eliminar-paciente = "eliminarPaciente"
+          />
+        </div>
+        <p v-else class="mt-20 text-2xl text-center">No hay pacientes</p>
+      </div>
     </div>
   </div>
 </template> 
--- admin-pacientes/src/components/Formulario.vue
+++ admin-pacientes/src/components/Formulario.vue
@@ -1,43 +1,122 @@
 <script setup>
-    import {ref} from 'vue'
-    const nombre = ref('Max')
-    const leerNombre = e =>{
-        nombre.value = e.target.value
+    import {computed, reactive} from 'vue'
+    import Alerta from './Alerta.vue'
+    // const propietario = ref('')
+    // const nombrePropietario = e =>{
+    //     propietario.value = e.target.value;
+    // }
+    const alerta = reactive({
+        tipo:'',
+        mensaje:''
+    })
+
+    
+
+    const emit = defineEmits(['update:nombre','update:propietario','update:email','update:alta','update:sintomas','guardar-paciente'])
+
+    const validar = () => {
+        // e.preventDefault();
+        if(Object.values(props).includes('')){
+            alerta.mensaje = 'Todos los campos son obligatorios'
+            alerta.tipo = 'error'
+            setTimeout(()=>{
+                Object.assign(alerta,{
+                    mensaje:'',
+                    tipo:''
+                })
+            },3000)
+            return
+        }
+        emit('guardar-paciente')
+        alerta.mensaje = 'Paciente Almacenado Correctamente'
+        alerta.tipo ='exito'
+
+        setTimeout(() => {
+            Object.assign(alerta,{
+                mensaje:'',
+                tipo:''
+            })
+        },3000)
     }
+
+    const props = defineProps({
+        nombre:{
+            type:String,
+            required:true
+        },
+        propietario:{
+            type:String,
+            required:true
+        },
+        email:{
+            type:String,
+            required:true
+        },
+        alta:{
+            type:String,
+            required:true
+        },
+        sintomas:{
+            type:String,
+            required:true
+        },
+        id:{
+            type:[String, null],
+            required:true
+        }
+    })
+
+    const editando = computed(()=>{
+        return props.id
+    })
 </script>
 
 <template>
     <div class="md:w-1/2">
         <h2 class="font-black text-3xl text-center">Seguimiento Pacientes</h2>
-
+        
         <p class="text-lg mt-5 text-center mb-10">
             Añade Pacientes y
-            <span class="text-indigo-600 font-bold">Administralos</span>
+            <span class="text-indigo-600 font-bold">Administralos</span>  
         </p>
-        <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
-            {{ nombre }}
+        <Alerta 
+            v-if="alerta.mensaje"
+            :alerta="alerta"
+        /> 
+        <form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+            @submit.prevent="validar"
+        
+        >
             <div class="mb-5">
+                
                 <label for="mascota" class="block text-gray-700 uppercase font-bold">
                     Nombre Mascota
                 </label>
                 <input id="mascota" type="text" placeholder="Nombre de la mascota"
                     class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
-                    :value = "nombre"
-                    v-on:input="leerNombre"/>
+                    :value="nombre"
+                    @input="$emit('update:nombre',$event.target.value)"
+                    />
             </div>
             <div class="mb-5">
                 <label for="propietario" class="block text-gray-700 uppercase font-bold">
                     Nombre Propietario
                 </label>
                 <input id="propietario" type="text" placeholder="Nombre propietario"
-                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+                    :value="propietario"
+                    @input="$emit('update:propietario',$event.target.value)"
+                    />
             </div>
             <div class="mb-5">
                 <label for="email" class="block text-gray-700 uppercase font-bold">
                     Email
                 </label>
                 <input id="email" type="text" placeholder="Email del propietario"
-                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" />
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+                    :value="email"
+                    @input="$emit('update:email',$event.target.value)"
+                    />
             </div>
             <div class="mb-5">
                 <label for="alta" class="block text-gray-700 uppercase font-bold">
@@ -47,6 +126,8 @@
                     id="alta"
                     type="date"
                     class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                    :value="alta"
+                    @input="$emit('update:alta',$event.target.value)"
                 />
             </div>
             <div class="mb-5">
@@ -58,11 +139,14 @@
                     type="text"
                     placeholder="Describe los sintomas"
                     class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"
-                />
+                    :value="sintomas"
+                    @input="$emit('update:sintomas',$event.target.value)"
+                    />
             </div>
             <input type="submit" class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700
             cursor-pointer transition-color"
-            value="Registrar paciente">
+            :value="[editando ? 'Guardar Cambios':'Registrar Paciente']"/>  <!--Tambien funcionaria solo con id en vez de editando, pero al parecer
+                                                                                 se hace con editando para ser mas organizado-->
 
         </form>
     </div>
