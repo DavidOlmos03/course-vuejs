92e863008ffdaa2b7d94394557fdd63a306c37ee
Juan David Ruiz Olmos <juanda_201001@hotmail.com>
2024-05-14 03:01:09 -0500

    text on modal bebidas-vue added

--- bebidas-vue/src/components/Modal.vue
+++ bebidas-vue/src/components/Modal.vue
@@ -6,6 +6,25 @@
 
     const modal = useModalStore()
     const bebidas = useBebidaStore()
+
+    const formatearIngredientes = ()=>{
+        const ingredientesDiv = document.createElement('DIV')
+
+        for (let i = 1; i <= 15; i++) {
+            if (bebidas.receta[`strIngredient${i}`]) {
+                const ingrediente = bebidas.receta[`strIngredient${i}`]
+                const cantidad = bebidas.receta[`strMeasure${i}`]
+
+                const ingredienteCantidad = document.createElement('P')
+                ingredienteCantidad.classList.add('text-lg', 'text-gray-500','font-extrabold')
+                ingredienteCantidad.textContent = `${ingrediente} - ${cantidad}`
+
+                ingredientesDiv.appendChild(ingredienteCantidad)
+            }
+            
+        }
+        return ingredientesDiv
+    }
 </script>
 
 <template>
@@ -24,7 +43,17 @@
                         {{ bebidas.receta.strDrink }}
                     </DialogTitle>
                     <img :src="bebidas.receta.strDrinkThumb" 
-                    :alt="'Imagen de '+ bebidas.receta.strDrink">
+                    :alt="'Imagen de '+ bebidas.receta.strDrink"
+                    class="mx-auto w-96"
+                    >
+                    <DialogTitle as="h3" class="text-gray-900 text-4xl font-extrabold my-5">
+                        Ingredientes y cantidades
+                    </DialogTitle>
+                    <div v-html="formatearIngredientes().outerHTML"></div>
+                    <DialogTitle as="h3" class="text-gray-900 text-4xl font-extrabold my-5">
+                       Instrucciones
+                    </DialogTitle>
+                    <p class="text-lg font-extrabold text-gray-500">{{ bebidas.receta.strInstructions }}</p>
                   </div>
                 </div>
                 <div class="mt-5 sm:mt-6 flex justify-between gap-4">
