bc9a1502b14ed337dd19c82b199db157df272d52
Juan David Ruiz Olmos <juanda_201001@hotmail.com>
2024-05-16 11:30:38 -0500

    login and home views added

---
+++ vue-bienes-raices/src/views/LoginView.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+</script>
+
+<template>
+
+    <v-card
+        flat
+        max-width="600"
+        class="mx-auto my-10"
+    >
+        <v-card-title
+            class = "text-h4 font-weight-bold pb-0"
+            tag = "h3"
+
+        >
+           Iniciar Sesión
+        </v-card-title>
+        <v-card-subtitle
+            class="text-h5"
+        >
+            Inicia Sesión con tu cuenta
+        </v-card-subtitle>
+
+        <v-form class="mt-5">
+            <v-text-field 
+                type="email"
+                label="Email"
+                bg-color="blue-grey-lighten-5"
+            />
+            <v-text-field 
+                type="password"
+                label="Pasword"
+                bg-color="blue-grey-lighten-5"
+            />
+            <v-btn
+                block
+                color="pink-accent-3"
+            >
+                Iniciar Sesión
+            </v-btn>
+        </v-form>
+    </v-card>
+</template>
