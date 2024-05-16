f347ae67168f37cacd123e357cea6d1ff6583cb9
Juan David Ruiz Olmos <juanda_201001@hotmail.com>
2024-05-16 15:03:13 -0500

    error control in login with firebase

--- vue-bienes-raices/src/stores/auth.js
+++ vue-bienes-raices/src/stores/auth.js
@@ -1,19 +1,21 @@
 import { defineStore } from 'pinia'
 import { useFirebaseAuth } from 'vuefire'
-
 import { signInWithEmailAndPassword }  from 'firebase/auth'
 
 export const useAuthStore = defineStore('auth', ()=>{
     const auth = useFirebaseAuth()
 
+    const errorCodes = {
+        'auth/invalid-credential' : 'Credenciales invalidas'
+    }
     const login = ({email,password})=>{
         signInWithEmailAndPassword(auth, email, password)
         .then((userCredential)=>{
             // const user = userCredential.user
             console.log(userCredential)
         })
-        .catch(error=>{
-            console.log(error.code)
+        .catch((error)=>{
+            console.log(errorCodes[error.code])
         })
     }
 
