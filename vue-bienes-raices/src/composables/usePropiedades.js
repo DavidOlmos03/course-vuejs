be79657164db58dfdc9f844ed80cd1edfa6ca299
Juan David Ruiz Olmos <juanda_201001@hotmail.com>
2024-05-18 19:09:11 -0500

    properties to propiedad added

--- vue-bienes-raices/src/composables/usePropiedades.js
+++ vue-bienes-raices/src/composables/usePropiedades.js
@@ -1,26 +1,25 @@
-import { computed } from 'vue'
+import { ref, computed } from 'vue'
 import {collection} from 'firebase/firestore'
 import {useFirestore, useCollection} from 'vuefire'
 
 
 export default function usePropiedades(){
+    const alberca = ref(false)
     // Conecta y hace la peticion a firebase
     const db = useFirestore()
     // Se accede a la información que se encuentra en firebase, el propiedadesCollection es por convencion de firebase
     const propiedadesCollection = useCollection(collection(db, 'propiedades'))
 
-    const propertyPrice = computed(()=>{
-        return (price)=>
-            // console.log(price)
-            Number(price).toLocaleString('en-US',{
-                style:'currency',
-                currency:'USD'
-            })
-        
+    const propiedadesFiltradas = computed(()=>{
+        return alberca.value ? 
+        propiedadesCollection.value.filter(propiedad => propiedad.alberca) : 
+        propiedadesCollection.value
     })
+
     return {
+        alberca,
         propiedadesCollection,
-        propertyPrice
+        propiedadesFiltradas
     }
 
 }
--- vue-bienes-raices/src/views/HomeView.vue
+++ vue-bienes-raices/src/views/HomeView.vue
@@ -1,11 +1,27 @@
 <script setup>
-
+    import usePropiedades from '../composables/usePropiedades'
+    import Propiedad from '../components/Propiedad.vue'
+    import {propertyPrice} from '../helpers'
+    const {alberca, propiedadesFiltradas} = usePropiedades()
 </script>
 
 <template>
     <div>
-        <h1 class="text-center text-h3 font-weight-bold my-5">Vue Bienes Raices</h1>
-        <h6>Under construction..</h6>
+        <h1 class="text-center text-h3 font-weight-bold my-5">Nuestras Propiedades</h1>
+        <v-card class="py-10">
+            <v-card-title class="text-h5">
+                Búsqueda
+                <v-checkbox label="Alberca" v-model="alberca" />
+            </v-card-title>
+            <v-row>
+                <Propiedad 
+                    v-for="propiedad in propiedadesFiltradas" :key="propiedad.id"
+                    :propiedad = "propiedad"
+                    :price="propertyPrice"
+                />
+            </v-row>
+        </v-card>
+        
     </div>
     
 </template>
